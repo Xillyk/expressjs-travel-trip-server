18c71cf32875f33691ad4fc67cafd281b1511127
Xillyk <47789885+Xillyk@users.noreply.github.com>
2025-02-02 21:01:31 +0700

    create endpoint for get trip data

--- routes/trip.ts
+++ routes/trip.ts
@@ -1,20 +1,30 @@
 import { Request, Response, Router } from "express";
+import TripsData from "../mock-trip.json";
 
 const router = Router();
 
-// * get all trips
-router.get("/", (_, res: Response) => {
-  res.send({
-    data: "all trips",
-  });
-});
+router.get("/", (req: Request, res: Response) => {
+  const keyword = req.query.keyword?.trim();
 
-// * get trip by keyword
-router.get("/:keyword", (req: Request, res: Response) => {
-  console.log(req.params);
-  res.send({
-    data: req.params,
-  });
+  if (!keyword) {
+    // * get all trips
+    res.status(200).send({
+      data: TripsData,
+    });
+  } else {
+    // * get trip by keyword
+    // ? better use elastic search for search engine
+    const findData = TripsData.trips.filter(
+      (trip) =>
+        trip.title.includes(keyword) ||
+        trip.description.includes(keyword) ||
+        trip.tags.filter((tag) => tag.includes(keyword)).length > 0
+    );
+
+    res.status(200).send({
+      data: findData,
+    });
+  }
 });
 
 export default router;

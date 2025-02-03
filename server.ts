533f8aa91ac6cfa5632af599da272713cbafa81d
Xillyk <47789885+Xillyk@users.noreply.github.com>
2025-02-03 11:42:25 +0700

    set cors to accept web localhost origin

--- server.ts
+++ server.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import tripRoute from "./routes/trip";
+import cors from "cors";
+
+const port = 9000;
 
 const app = express();
-const port = 9000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello Traveler!");
